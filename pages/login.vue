62156996301ece7a9bb6fd8318acb7808aff5090
Sss-Okubo <bonchan_purin@yahoo.co.jp>
2024-03-27 21:02:30 +0900

    ログイン画面のバリデーション追加

--- pages/login.vue
+++ pages/login.vue
@@ -4,13 +4,16 @@
       <v-card-text>
         <h2 class="text-center"> ログイン</h2>
       </v-card-text>
-      <v-text-field density="compact" placeholder="Email address" prepend-inner-icon="mdi-email-outline"
-        variant="outlined" v-model="email" type="email" required>
-      </v-text-field>
-      <v-text-field density="compact" placeholder="Enter your password" prepend-inner-icon="mdi-lock-outline"
-        variant="outlined" @click:append-inner="visible = !visible" v-model="password" type="password" required>
-      </v-text-field>
-      <v-btn block class="mb-8" color="red" dark size="large" variant="tonal" @click="login">
+      <v-text-field v-model="email" :rules="emailRules" label="E-mail" required @focus="hideAlert"></v-text-field>
+
+      <v-text-field v-model="password" :rules="passwordRules" label="password" required
+        @focus="hideAlert"></v-text-field>
+
+      <v-alert v-if="showAlert" border="right" colored-border type="error" elevation="2" v-model="password"
+        style="font-size: 10px;" class="mt-5">
+        {{ alertMessage }}
+      </v-alert>
+      <v-btn block class="mt-8" color="red" dark size="large" @click="login">
         ログイン
       </v-btn>
       <div class="d-flex justify-center mt-5">
@@ -28,15 +31,28 @@ export default {
   layout: 'other',
   data() {
     return {
-      email: null,
-      password: null,
+      valid: true,
+      showAlert: false,
+      alertMessage: null,
+      email: "",
+      emailRules: [
+        v => !!v || 'E-mail is required',
+        v => /.+@.+\..+/.test(v) || 'E-mail must be valid',
+      ],
+      password: "",
+      passwordRules: [
+        v => !!v || 'Password is required',
+        v => (v && v.length >= 6) || 'Password must be at least 6 characters',
+      ],
     }
   },
   methods: {
     login() {
+      
       if (!this.email || !this.password) {
-        alert('メールアドレスまたはパスワードが入力されていません。')
-        return
+        this.alertMessage = "メールアドレスまたはパスワードが入力されていません。";
+        this.showAlert = true;
+        return;
       }
       firebase
         .auth()
@@ -46,24 +62,35 @@ export default {
         })
         .catch((error) => {
           switch (error.code) {
-            case 'auth/invalid-email':
-              alert('メールアドレスの形式が違います。')
-              break
-            case 'auth/user-disabled':
-              alert('ユーザーが無効になっています。')
-              break
-            case 'auth/user-not-found':
-              alert('ユーザーが存在しません。')
-              break
-            case 'auth/wrong-password':
-              alert('パスワードが間違っております。')
-              break
+            case "auth/invalid-email":
+              this.alertMessage = "メールアドレスの形式が違います。";
+              this.showAlert = true;
+              break;
+            case "auth/weak-password":
+              this.alertMessage = "パスワードは6文字以上で入力してください。";
+              this.showAlert = true;
+              break;
             default:
-              alert('エラーが起きました。しばらくしてから再度お試しください。')
-              break
+              this.alertMessage = "ログインできません。メールアドレスまたはパスワードを確認してください。";
+              this.showAlert = true;
+              console.log(error.code)
+              break;
           }
         })
     },
+    validate() {
+      this.$refs.form.validate()
+    },
+    reset() {
+      this.$refs.form.reset()
+    },
+    resetValidation() {
+      this.$refs.form.resetValidation()
+    },
+    hideAlert() {
+      // テキストフィールドがフォーカスを受け取ったときにアラートを非表示にする
+      this.showAlert = false;
+    }
   },
 }
 </script>
